fa8b5bdc7948edd441df2ff13593155f4049c291
Henry Kwok <henryk@nachocove.com>
2014-11-24 10:56:58 -0800

    Reformat tabs to spaces.

--- Lucene.Cli/Emails.cs
+++ Lucene.Cli/Emails.cs
@@ -6,85 +6,86 @@ using NachoCore.Index;
 
 namespace Lucene.Cli
 {
-	public partial class LuceneCli
-	{
-		private static List<string> _EmailPaths;
-		private static List<string> EmailPaths {
-			get {
-				if (null == _EmailPaths) {
-					_EmailPaths = new List<string> ();
-				}
-				return _EmailPaths;
-			}
-		}
+    public partial class LuceneCli
+    {
+        private static List<string> _EmailPaths;
 
-		private static void GetAllFiles (string[] args)
-		{
-			foreach (var path in args) {
-				// Check if the path exist
-				if (File.Exists (path)) {
-					EmailPaths.Add (path);
-					continue;
-				}
+        private static List<string> EmailPaths {
+            get {
+                if (null == _EmailPaths) {
+                    _EmailPaths = new List<string> ();
+                }
+                return _EmailPaths;
+            }
+        }
 
-				if (Directory.Exists (path)) {
-					var files = Directory.GetFiles (path);
-					if (0 < files.Length) {
-						GetAllFiles (files);
-					}
-					var dirs = Directory.GetDirectories (path);
-					if (0 < dirs.Length) {
-						GetAllFiles (dirs);
-					}
-					continue;
-				}
+        private static void GetAllFiles (string[] args)
+        {
+            foreach (var path in args) {
+                // Check if the path exist
+                if (File.Exists (path)) {
+                    EmailPaths.Add (path);
+                    continue;
+                }
 
-				Log.Warn ("{0} is neither a file nor a directory", path);
-			}
-		}
+                if (Directory.Exists (path)) {
+                    var files = Directory.GetFiles (path);
+                    if (0 < files.Length) {
+                        GetAllFiles (files);
+                    }
+                    var dirs = Directory.GetDirectories (path);
+                    if (0 < dirs.Length) {
+                        GetAllFiles (dirs);
+                    }
+                    continue;
+                }
 
-		private static void AddEmails (string[] paths)
-		{
-			long bytesIndexed = 0;
-			GetAllFiles (paths);
-			Log.Info ("Adding {0} emails", EmailPaths.Count);
-			using (var index = new Index (IndexDirectory)) {
-				index.BeginAddTransaction ();
-				foreach (var emailPath in EmailPaths) {
-					Log.Debug ("Add {0}", emailPath);
-					bytesIndexed += index.BatchAdd (emailPath, "message", emailPath);
-				}
-				index.EndAddTransaction ();
-			}
-			Log.Info ("{0} bytes indexed", bytesIndexed);
-		}
+                Log.Warn ("{0} is neither a file nor a directory", path);
+            }
+        }
 
-		private static void RemoveEmails (string[] paths)
-		{
-			GetAllFiles (paths);
-			Log.Info ("Removing {0} emails", EmailPaths.Count);
-			int count = 0;
-			using (var index = new Index (IndexDirectory)) {
-				index.BeginRemoveTransaction ();
-				foreach (var emailPath in EmailPaths) {
-					if (!index.Remove ("message", emailPath)) {
-						count += 1;
-					}
-				}
-				index.EndRemoveTransaction ();
-			}
-			Log.Info ("{0} emails removed", count);
-		}
+        private static void AddEmails (string[] paths)
+        {
+            long bytesIndexed = 0;
+            GetAllFiles (paths);
+            Log.Info ("Adding {0} emails", EmailPaths.Count);
+            using (var index = new Index (IndexDirectory)) {
+                index.BeginAddTransaction ();
+                foreach (var emailPath in EmailPaths) {
+                    Log.Debug ("Add {0}", emailPath);
+                    bytesIndexed += index.BatchAdd (emailPath, "message", emailPath);
+                }
+                index.EndAddTransaction ();
+            }
+            Log.Info ("{0} bytes indexed", bytesIndexed);
+        }
 
-		private static void SearchEmails (string search)
-		{
-			using (var index = new Index (IndexDirectory)) {
-				int n = 1;
-				foreach (var match in index.Search(search)) {
-					Log.Info ("{0} {1}", n, match.Id);
-					n += 1;
-				}
-			}
-		}
-	}
+        private static void RemoveEmails (string[] paths)
+        {
+            GetAllFiles (paths);
+            Log.Info ("Removing {0} emails", EmailPaths.Count);
+            int count = 0;
+            using (var index = new Index (IndexDirectory)) {
+                index.BeginRemoveTransaction ();
+                foreach (var emailPath in EmailPaths) {
+                    if (!index.Remove ("message", emailPath)) {
+                        count += 1;
+                    }
+                }
+                index.EndRemoveTransaction ();
+            }
+            Log.Info ("{0} emails removed", count);
+        }
+
+        private static void SearchEmails (string search)
+        {
+            using (var index = new Index (IndexDirectory)) {
+                int n = 1;
+                foreach (var match in index.Search(search)) {
+                    Log.Info ("{0} {1}", n, match.Id);
+                    n += 1;
+                }
+            }
+        }
+    }
 }
--- Lucene.Cli/Program.cs
+++ Lucene.Cli/Program.cs
@@ -7,81 +7,80 @@ using Lucene.Net.Store;
 
 namespace Lucene.Cli
 {
-	public partial class LuceneCli
-	{
-		private static string IndexDirectory = "lucene.index";
+    public partial class LuceneCli
+    {
+        private static string IndexDirectory = "lucene.index";
 
-		private static void Help (int exitCode = 1)
-		{
-			Console.WriteLine ("USAGE: mono Lucene.Cli.exe [COMMANDS] [PARAMS...]\n");
-			Console.WriteLine ("add [directories/files] - Add emails or directories of emails");
-			Console.WriteLine ("remove [directories/files] - Remove emails");
-			Console.WriteLine ("search [PATTERN]");
-			Environment.Exit (exitCode);
-		}
+        private static void Help (int exitCode = 1)
+        {
+            Console.WriteLine ("USAGE: mono Lucene.Cli.exe [COMMANDS] [PARAMS...]\n");
+            Console.WriteLine ("add [directories/files] - Add emails or directories of emails");
+            Console.WriteLine ("remove [directories/files] - Remove emails");
+            Console.WriteLine ("search [PATTERN]");
+            Environment.Exit (exitCode);
+        }
 
-		private static void ParserOptions (string[] args)
-		{
-			if (0 == args.Length) {
-				Help (0);
-			}
+        private static void ParserOptions (string[] args)
+        {
+            if (0 == args.Length) {
+                Help (0);
+            }
 
-			try {
-				int index = 0;
-				bool done = false;
+            try {
+                int index = 0;
+                bool done = false;
 
-				// Parse general options
-				while (!done && (index < args.Length)) {
-					switch (args [index]) {
-					case "-d":
-						index += 1; // consume -d
-						Log.Debug ("Changing working directory to {0}", args [index]);
-						System.IO.Directory.SetCurrentDirectory (args [index]);
-						index += 1; // consume root directory
-						break;
-					case "-i":
-						index += 1; // consume -i
-						IndexDirectory = args [index];
-						index += 1; // consume index file path
-						break;
-					case "-v":
-						index += 1; // consume -v
-						Log.DebugEnabled = true;
-						break;
-					default:
-						done = true;
-						break;
-					}
-				}
+                // Parse general options
+                while (!done && (index < args.Length)) {
+                    switch (args [index]) {
+                    case "-d":
+                        index += 1; // consume -d
+                        Log.Debug ("Changing working directory to {0}", args [index]);
+                        System.IO.Directory.SetCurrentDirectory (args [index]);
+                        index += 1; // consume root directory
+                        break;
+                    case "-i":
+                        index += 1; // consume -i
+                        IndexDirectory = args [index];
+                        index += 1; // consume index file path
+                        break;
+                    case "-v":
+                        index += 1; // consume -v
+                        Log.DebugEnabled = true;
+                        break;
+                    default:
+                        done = true;
+                        break;
+                    }
+                }
 
-				// Check the command
-				var command = args [index].ToLower ();
-				string[] parameters = new string[args.Length - 1 - index];
-				Array.Copy (args, index + 1, parameters, 0, args.Length - 1 - index);
-				switch (command) {
-				case "add":
-					AddEmails (parameters);
-					break;
-				case "remove":
-					RemoveEmails (parameters);
-					break;
-				case "search":
-					SearchEmails (parameters[0]);
-					break;
-				default:
-					Console.WriteLine ("ERROR: unknown command {0}", args [index]);
-					Help ();
-					break;
-				}
-			}
-			catch (IndexOutOfRangeException) {
-				Help ();
-			}
-		}
+                // Check the command
+                var command = args [index].ToLower ();
+                string[] parameters = new string[args.Length - 1 - index];
+                Array.Copy (args, index + 1, parameters, 0, args.Length - 1 - index);
+                switch (command) {
+                case "add":
+                    AddEmails (parameters);
+                    break;
+                case "remove":
+                    RemoveEmails (parameters);
+                    break;
+                case "search":
+                    SearchEmails (parameters [0]);
+                    break;
+                default:
+                    Console.WriteLine ("ERROR: unknown command {0}", args [index]);
+                    Help ();
+                    break;
+                }
+            } catch (IndexOutOfRangeException) {
+                Help ();
+            }
+        }
 
-		public static void Main (string[] args)
-		{
-			ParserOptions (args);
-		}
-	}
+        public static void Main (string[] args)
+        {
+            ParserOptions (args);
+        }
+    }
 }
